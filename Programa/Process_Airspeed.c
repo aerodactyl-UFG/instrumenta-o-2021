7bd8ff1b532c1f49d85c763e54cc5542de1262d8
aerodactyl-UFG <aerodactylufg@gmail.com>
2021-08-05 21:04:05 -0300

    config for airspeed calib

--- Programa/Process_Airspeed.c
+++ Programa/Process_Airspeed.c
@@ -44,8 +44,8 @@ void process_airspeed () {
     temperature = ((200.0f * dT_raw) / 2047) - 50;
     float diff_press_PSI = -((dp_raw - 0.1f * 16383) * (P_max - P_min) / (0.8f * 16383) + P_min);
     float diff_press_pa_raw = diff_press_PSI * PSI_to_Pa;
-    airspeed = calc_indicated_airspeed(diff_press_pa_raw);
-
+//    airspeed = calc_indicated_airspeed(diff_press_pa_raw);
+    airspeed = diff_press_pa_raw;
     time_out = ( double(Timer) - double(time_zero) ) / 1000000;	
 	
 }
--- Programa/main.cpp
+++ Programa/main.cpp
@@ -21,24 +21,24 @@ int main(){
 //		printf("\nEstado: %d", estado);
 		if (estado == 1){
 		    UpdateManager(1);	
-		    std::thread t1 (read_imu, &estado);
+//		    std::thread t1 (read_imu, &estado);
 		    std::thread t2 (read_pitot, &estado);
 
-		    std::cout << "thread t1 id= " << t1.get_id() << std::endl;	
+//		    std::cout << "thread t1 id= " << t1.get_id() << std::endl;	
 		    std::cout << "thread t2 id= " << t2.get_id() << std::endl;
 
-  		    t1.join();
+//  		    t1.join();
 		    t2.join();
 		    }
 
 		else	if (UpdateManager(2) ){	
-		    	std::thread t3 (process_imu);
+//		    	std::thread t3 (process_imu);
 		    	std::thread t4 (process_pitot);
 
-		    	std::cout << "thread t3 id= " << t3.get_id() << std::endl;	
+//		    	std::cout << "thread t3 id= " << t3.get_id() << std::endl;	
 		    	std::cout << "thread t4 id= " << t4.get_id() << std::endl;
 
-		    	t3.join();
+//		    	t3.join();
 		    	t4.join();
 		    }
 
